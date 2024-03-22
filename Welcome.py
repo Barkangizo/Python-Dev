75ad1aac74de54d54311136a1114b8728b3a8ff4
Barkangizo <164011902+Barkangizo@users.noreply.github.com>
2024-03-22 16:11:56 +0100

    Last update
    Modify line 25 34

--- Welcome.py
+++ Welcome.py
@@ -8,8 +8,27 @@ if 5 > 2:
 x = 5
 y = 'John'
 
-print(x)
-print(y)
+#print(x)
+#print(y)
 
 print(type(x))
 print(type(y))
+
+x = 'abubakar'
+x = 4
+
+print(x)
+#print(X)
+
+Movies = ['tears of the sun', 'coming to america', 'goodby tgo nigeria']
+
+x, y, z = Movies
+print(y)
+print(y)
+print(z)
+
+myName = 'abubakar' #camel casing
+MyDateOfBirth = '12-06-00'#Pascal case
+my_total_score = 96 #snake case
+
+print(myName, my_total_score)
